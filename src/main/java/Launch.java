3ad8da7a19408469948662e74186478e21ef5fe3
Fadwa2020git <fadwa_fazouane@hotmail.fr>
2021-01-09 22:22:27 +0100

    Commit 01 09 2021 : adding the class Game, chaging the main class

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -3,100 +3,51 @@ package main.java;
 import java.util.Scanner;
 
 public class Launch {
-    public static Player currentPlayer = new Player();
 
     public static void main(String[] args) {
-        Player person = new Player();
-        Player program = new Player();
+        Game game = new Game();
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Scanner entry;
         char choice;
-        char[][] board = new char[3][3];
+        int line, column;
+
+        //Asking the player to choose his mark
         System.out.println("Welcome to Tic Tac Toe\n Please enter your choice between X and O");
-        Scanner entry = new Scanner(System.in);
-        choice = entry.nextLine().charAt(0);
-        person.setContent(choice);
+        //Repeating the process until the user chooses either x or o
+        do {
+            entry = new Scanner(System.in);
+            choice = entry.nextLine().charAt(0);
+        } while (entry.nextLine().charAt(0) !='x' && entry.nextLine().charAt(0) != 'o');
+        player1.setContent(choice);
+        game.setCurrentPlayer(player1);
         if (choice == 'x')
-            program.setContent('o');
+            player2.setContent('o');
         else if (choice == 'o')
-            program.setContent('x');
-        initializeBoard(board);
-        printBoard(board);
-    }
-
-    public static void initializeBoard(char[][] board) {
-        for (int line=0;line<3;line++) {
-            for (int column=0; column<3; column++) {
-                board[line][column] = '-';
-            }
+            player2.setContent('x');
+        //Playing the game : loop that goes on until the board is full or there's a winner
+        do {
+            System.out.println("Current Board Layout :");
+            game.printBoard();
+            //Place mark in an empty cell
+            do {
+                System.out.println("Player " + game.getCurrentPlayer().getContent() + " please choose a cell in which you play\n" +
+                        "Please enter the line then the column whithin the bounds >=0 and <=2");
+                line = entry.nextInt()-1;
+                column = entry.nextInt()-1;
+            } while (!game.fillCellContent(line, column));
+            //Change players
+            game.switchPlayer();
+        } while (!game.isBoardFull() && !game.isWin());
+        if (game.isBoardFull() && !game.isWin()) {
+            System.out.println("No Winner");
         }
-    }
-
-    public static void printBoard(char[][] board) {
-        System.out.println("-------------");
-        for (int line=0;line<3;line++) {
-            System.out.print("| ");
-            for (int column=0; column<3; column++) {
-                System.out.print(board[line][column] + " |");
-                System.out.print(" ");
-            }
-            System.out.println();
-            System.out.println("-------------");
+        if (game.isWin()) {
+            game.switchPlayer();
+            System.out.println("Congrats Player " + game.getCurrentPlayer().getContent());
         }
-    }
-
-    public static void switchPlayer() {
-        if (currentPlayer.getContent() == 'x')
-            currentPlayer.setContent('o');
-        else if (currentPlayer.getContent() == 'o')
-            currentPlayer.setContent('x');
-    }
-
-    public boolean isFull(char[][] board) {
-        for (int line=0;line<3;line++) {
-            for (int column=0; column<3; column++) {
-                if (board[line][column] == '\0'
-                        || board[line][column] == ' '
-                        || board[line][column] == '-')
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public static char[][] fillContent(char[][] board, int line, int column) throws Exception {
-        try {
-            if (line>=3 || line<0 || column>=3 || column<0)
-                throw new Exception();
-            board[line][column] = currentPlayer.getContent();
-        } catch (Exception e) {
-            System.out.println("Value out of bounds");
-        }
-        return board;
-    }
-
-    public boolean isLineTriple(char[][] board) {
-        boolean tripleValues = false;
-        char cellContent;
-        for (int line=0;line<3;line++) {
-            cellContent = board[line][0];
-            tripleValues = true;
-            for (int column=1; column<3; column++) {
-                if (board[line][column] != cellContent)
-                    tripleValues = false;
-            }
-            if (tripleValues)
-                return tripleValues;
-        }
-        return tripleValues;
-    }
-
-    public boolean isColumnTriple(char[][] board) {
-        boolean tripleValues = false;
-        return tripleValues;
-    }
-
-    public boolean isDiagonalTriple(char[][] board) {
-        boolean tripleValues = false;
-        return tripleValues;
+        entry.close();
+        System.out.println("End of the Game, thanks for playing");
     }
 
 }
